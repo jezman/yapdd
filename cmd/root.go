8707fdcae9c80697360d48623729895f11b787ca
jezman <jez.studio@gmail.com>
2018-02-11 13:57:07 +0300

    Change "show" logic. Migrate to root cmd.

--- cmd/root.go
+++ cmd/root.go
@@ -3,13 +3,13 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/jezman/yapdd/render"
 	"github.com/spf13/cobra"
 )
 
 var (
-	account string
-	domain  string
 	verbose bool
 )
 
@@ -17,6 +17,24 @@ var (
 var rootCmd = &cobra.Command{
 	Use:   "yapdd",
 	Short: "Command line application for administration Yandex PDD.",
+	Long: `YaPDD - Command line application for administration Yandex PDD.
+
+Example:
+  yapdd                     Domains list.
+  yapdd example.com         List of accounts in domain.
+  yapdd acc@example.com     Count of unread emails in account.`,
+	Args: cobra.RangeArgs(0, 1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			render.Domains(verbose)
+		} else {
+			if strings.Contains(args[0], "@") {
+				render.CountOfUnreadMail(args[0])
+			} else {
+				render.Accounts(args[0], verbose)
+			}
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -30,4 +48,5 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	// rootCmd.Flags().BoolVarP(&status, "status", "s", false, "show status")
 }
