c2c5f0ec3b7ed770ef60788eaa6465a530df1bd3
jezman <jez.studio@gmail.com>
2018-02-12 13:49:10 +0300

    Remove params from urls.

--- pdd/pdd.go
+++ pdd/pdd.go
@@ -11,14 +11,14 @@ const (
 	// BaseURL API version 2
 	BaseURL = "https://pddimp.yandex.ru/api2"
 
-	DomainsList      = BaseURL + "/admin/domain/domains?&on_page=20"
+	DomainsList      = BaseURL + "/admin/domain/domains"
 	DomainAdd        = BaseURL + "/admin/domain/register"
 	DomainStatus     = BaseURL + "/admin/domain/registration_status"
 	DomainConfig     = BaseURL + "/admin/domain/details"
 	DomainDelete     = BaseURL + "/admin/domain/delete"
 	DomainSetCountry = BaseURL + "/admin/domain/settings/set_country"
 
-	AccountsList        = BaseURL + "/admin/email/list?&on_page=1000&domain="
+	AccountsList        = BaseURL + "/admin/email/list"
 	AccountAdd          = BaseURL + "/admin/email/add"
 	AccountDelete       = BaseURL + "/admin/email/del"
 	AccountUnreadEmails = BaseURL + "/admin/email/counters"
