f8ed7c19aeffde2431b5c4d0c356f65d8d8008a5
jezman <dev.jezman@gmail.com>
2018-02-16 13:06:12 +0300

    Refactoring add_cmd.

--- render/add_cmd.go
+++ render/add_cmd.go
@@ -3,33 +3,26 @@ package render
 import (
 	"fmt"
 
-	"github.com/jezman/yapdd/models"
+	"github.com/jezman/yapdd/utils"
 )
 
 // AddDomain print domain add result.
 func AddDomain(domainName string) {
-	domain := &models.Domain{}
-	json, err := domain.Add(domainName)
+	domain, err := domain.Add(domainName)
 
-	if err != nil {
+	if err = utils.ErrorCheck(domain.Success, domain.Error, err); err != nil {
 		fmt.Println(err)
-	} else if json.Success != "ok" {
-		fmt.Printf("Error: %s\n", json.Error)
 	} else {
-		fmt.Printf("Domain %s has been successfully connected.", json.Domain)
+		fmt.Printf("Domain %s has been successfully connected.", domain.Domain)
 	}
 }
 
 // AddAccount print account add result.
 func AddAccount(accountName string) {
-	account := &models.Account{}
-	json, err := account.Add(accountName)
+	account, err := account.Add(accountName)
 
-	if err != nil {
+	if err = utils.ErrorCheck(account.Success, account.Error, err); err != nil {
 		fmt.Println(err)
-	} else if json.Success != "ok" {
-		fmt.Printf("Error: %s\n", json.Error)
-	} else {
-		fmt.Printf("Account '%s' was successfully added.\n", json.Login)
+		fmt.Printf("Account '%s' was successfully added.\n", account.Login)
 	}
 }
