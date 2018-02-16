c334314edd80cb3b546959b06f4b633d1ae70344
jezman <dev.jezman@gmail.com>
2018-02-16 13:08:21 +0300

    Refactoring remove_cmd.

--- render/remove_cmd.go
+++ render/remove_cmd.go
@@ -3,35 +3,29 @@ package render
 import (
 	"fmt"
 
-	"github.com/jezman/yapdd/models"
+	"github.com/jezman/yapdd/utils"
 )
 
 // RemoveDomain print result of domain removed.
 func RemoveDomain(domainName string) {
-	domain := &models.Domain{}
-	json, err := domain.Remove(domainName)
+	domain, err := domain.Remove(domainName)
 
-	if err != nil {
+	if err = utils.ErrorCheck(domain.Success, domain.Error, err); err != nil {
 		fmt.Println(err)
-	} else if json.Success != "ok" {
-		fmt.Printf("Error: %s\n", json.Error)
 	} else {
-		fmt.Printf("Domain '%s' has been successfully removed.\n", json.Domain)
+		fmt.Printf("Domain '%s' has been successfully removed.\n", domain.Domain)
 	}
 
 }
 
 // RemoveAccount print result of account removed.
 func RemoveAccount(accountName string) {
-	account := &models.Account{}
-	json, err := account.Remove(accountName)
+	account, err := account.Remove(accountName)
 
-	if err != nil {
+	if err = utils.ErrorCheck(account.Success, account.Error, err); err != nil {
 		fmt.Println(err)
-	} else if json.Success != "ok" {
-		fmt.Printf("Error: %s\n", json.Error)
 	} else {
-		fmt.Printf("Account '%s' has been successfully removed.\n", json.Login)
+		fmt.Printf("Account '%s' has been successfully removed.\n", account.Login)
 	}
 
 }
